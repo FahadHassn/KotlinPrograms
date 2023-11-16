5446f35b992070392288d4e6bf64e030cd117c53
fahadhassan14 <fahadhassan@mobizion.com>
2023-11-16 16:24:46 +0500

    changes in constructor

---
+++ Practice16Nov/src/main/kotlin/Polymorphism.kt
@@ -0,0 +1,17 @@
+fun main(){
+    println(sum(2,3))
+}
+
+fun sum(){
+    val a =5
+    val b =7
+    println(a+b)
+}
+
+fun sum(a: Int, b: Int) : Int{
+    return a+b
+}
+
+fun sum(a: Double, b: Double) : Double{
+    return a+b
+}
