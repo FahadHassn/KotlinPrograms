4ef9a0b9c514eaa43444d3c39bb615b0e836bb36
fahadhassan14 <fahadhassan@mobizion.com>
2023-12-02 17:44:27 +0500

    Coroutines , PropertiesWithoutInitializeInClass

---
+++ Practice2Dec/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.utils.IMPLEMENTATION
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    IMPLEMENTATION("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
